c823f69cc843102694f9134a6076eac01179d10f
Joao Cardoso <jaliborc@arenajunkies.com>
2021-03-14 22:48:01 +0000

    Added copyright notice like all other libraries

--- Poncho-2.0.lua
+++ Poncho-2.0.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2008-2021 João Cardoso
+Copyright 2011-2021 João Cardoso
 Poncho is distributed under the terms of the GNU General Public License (or the Lesser GPL).
 This file is part of Poncho.
 
